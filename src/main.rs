ebb55ca24c04a9793bc67c7590af84b43b347093
Anton Whalley <antonwhalley@yahoo.com>
2023-12-26 11:44:59 +0000

    add:sentence embeddings
    Signed-off-by: Anton Whalley <antonwhalley@yahoo.com>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use polars::io::mmap::MmapBytesReader;
 use polars::prelude::*;
-use std::fs;
+use rust_bert::pipelines::sentence_embeddings::{
+    SentenceEmbeddingsBuilder, SentenceEmbeddingsModelType,
+};
 use std::io::Cursor;
 use std::path::Path;
-
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
 async fn fetch_url(url: String, file_name: String) -> Result<()> {
@@ -16,46 +16,99 @@ async fn fetch_url(url: String, file_name: String) -> Result<()> {
 
 #[tokio::main]
 async fn main() {
+    // Get Files test and training data
     let questions_test_filename = "./questions_test.csv";
     let questions_train_filename = "./questions_train.csv";
     let questions_test_url = "https://raw.github.com/IBM/watson-machine-learning-samples/master/cloud/data/RAG/questions_test.csv";
     let questions_train_url = "https://raw.github.com/IBM/watson-machine-learning-samples/master/cloud/data/RAG/questions_train.csv";
 
-    if Path::new(questions_test_filename).exists() {
-        fs::remove_file(questions_test_filename).expect("could not remove questions_test_filename");
+    if !Path::new(questions_test_filename).exists() {
+        fetch_url(
+            questions_test_url.to_string(),
+            questions_test_filename.to_string(),
+        )
+        .await
+        .unwrap();
     }
-    if Path::new(questions_train_filename).exists() {
-        fs::remove_file(questions_train_filename)
-            .expect("could not remove questions_train_filename");
+    if !Path::new(questions_train_filename).exists() {
+        fetch_url(
+            questions_train_url.to_string(),
+            questions_train_filename.to_string(),
+        )
+        .await
+        .unwrap();
     }
-    fetch_url(
-        questions_test_url.to_string(),
-        questions_test_filename.to_string(),
-    )
-    .await
-    .unwrap();
 
-    fetch_url(
-        questions_train_url.to_string(),
-        questions_train_filename.to_string(),
-    )
-    .await
-    .unwrap();
-    let file_test = std::fs::File::open(questions_test_filename).unwrap();
-    let file_train = std::fs::File::open(questions_train_filename).unwrap();
-    let file_test = Box::new(file_test) as Box<dyn MmapBytesReader>;
-    let file_train = Box::new(file_train) as Box<dyn MmapBytesReader>;
+    // Load files into MmapBytesReaders
+    // let file_test = std::fs::File::open(questions_test_filename).unwrap();
+    // let file_train = std::fs::File::open(questions_train_filename).unwrap();
+    // let file_test = Box::new(file_test) as Box<dyn MmapBytesReader>;
+    // let file_train = Box::new(file_train) as Box<dyn MmapBytesReader>;
 
-    let _test_reader = CsvReader::new(file_test)
+    let test_reader = LazyCsvReader::new(questions_test_filename)
         .with_separator(b',')
         .has_header(true)
-        .with_chunk_size(10)
-        .batched_mmap(None)
+        .finish()
         .unwrap();
-    let _training_reader = CsvReader::new(file_train)
+    let training_reader = LazyCsvReader::new(questions_train_filename)
         .with_separator(b',')
         .has_header(true)
-        .with_chunk_size(10)
-        .batched_mmap(None)
+        .finish()
         .unwrap();
+    let out = test_reader
+        .clone()
+        .select([col("*")])
+        .limit(5)
+        .collect()
+        .unwrap();
+    // Query training reader to print data
+    println!("{}", out);
+
+    // Load Knowledge base
+    let documents_filename = "./psgs.tsv";
+    let documents_url =
+        "https://raw.github.com/IBM/watson-machine-learning-samples/master/cloud/data/RAG/psgs.tsv";
+    if !Path::new(documents_filename).exists() {
+        fetch_url(documents_url.to_string(), documents_filename.to_string())
+            .await
+            .unwrap();
+    }
+
+    // the document does contain a header so should it be included in the rows?
+
+    let documents = LazyCsvReader::new(documents_filename)
+        .with_separator(b'\t')
+        .has_header(true)
+        .finish()
+        .unwrap();
+    let out = documents
+        .clone()
+        .select([col("*")])
+        .limit(5)
+        .collect()
+        .unwrap();
+    println!("{}", out);
+
+    let model = SentenceEmbeddingsBuilder::remote(SentenceEmbeddingsModelType::AllMiniLmL6V2)
+        .create_model()?;
+
+    // Define input
+    let sentences = ["this is an example sentence", "each sentence is converted"];
+
+    // Generate Embeddings
+    let embeddings = model.encode(&sentences)?;
+    println!("{embeddings:?}");
+
+    // Create an embedding function
+    // https://github.com/guillaume-be/rust-bert/blob/1f4d344668232da8e669e7fea1391c8829d5d1e3/examples/sentence_embeddings.rs
+
+    // Define Model on watsonx ???
+
+    // Connect to elastic instance
+
+    // Create elasticsearch store in connector
+
+    // Embed and index documents
+
+    // Generate a retrieval-augmented response to a question
 }
